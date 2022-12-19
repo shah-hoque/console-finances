10911a3c48febcf0a9cafdcd6b46550f28094e25
shah_hoque1 <shah.hoque10@gmail.com>
2022-12-19 14:05:18 +0000

    all sections done w/ README

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ var netMonthsInData = 0
 for (var i = 0; i < inputData.length; i++) {
     netMonthsInData++
 }
-console.log("Net months in data = " + netMonthsInData)
 
 ///////////////////////////////////////////////////////////////
 
@@ -17,7 +16,6 @@ var netProfitOrLoss = 0
 for (var i = 0; i < inputData.length; i++) {
     netProfitOrLoss = netProfitOrLoss + inputData[i][1]
 }
-console.log("Net profit ot loss = " + netProfitOrLoss)
 
 ///////////////////////////////////////////////////////////////
 
@@ -27,16 +25,40 @@ var avgMonthlyChange = 0
 for (var i = 0; i < inputData.length-1; i++) {
     avgMonthlyChange = avgMonthlyChange + (inputData[i+1][1] - inputData[i][1])
 }
-console.log("sum of months changes = " + avgMonthlyChange)
-
 avgMonthlyChange = avgMonthlyChange / netMonthsInData
-
-console.log(avgMonthlyChange)
-// round up data
 
 ///////////////////////////////////////////////////////////////
 
-// Greatest increase in profits
+// Greatest increase in profits & greatest decrease in profits
+
+var avgMonthlyChangeAsArray = []
+var listOfMonths = []
+
+
+for (var i = 0; i < inputData.length-1; i++) {
+    avgMonthlyChangeAsArray.push(inputData[i+1][1] - inputData[i][1]);
+    listOfMonths.push(inputData[i+1][0])
+}
+
+var TopIncreaseInProfit = Math.max(...avgMonthlyChangeAsArray);
+var TopDecreaseInProfit = Math.min(...avgMonthlyChangeAsArray);
+
+var maxIndex = avgMonthlyChangeAsArray.indexOf(TopIncreaseInProfit)
+var minIndex = avgMonthlyChangeAsArray.indexOf(TopDecreaseInProfit)
+
+
+/////////////////////////////////////////////////////////////
+
+// Print out report
+console.log("Financial Analysis")
+console.log("----------------------------")
+console.log("Total Months: " + netMonthsInData)
+console.log("Total: " + "$" + netProfitOrLoss)
+console.log("Greatest Increase in Profits: " + listOfMonths[maxIndex] + " " + "($" + TopIncreaseInProfit + ")" )
+console.log("Greatest Decrease in Profits: " + listOfMonths[minIndex] + " " + "($" + TopDecreaseInProfit + ")" )
+
+
+
 
 
 
